109eff4db39fc7073bed64c7cd383310bd631e11
Chris McCandlish <cmccand@gmail.com>
2015-02-17 00:21:07 -0500

    committing collection

--- collection.js
+++ collection.js
@@ -2,3 +2,7 @@ var Posts = Backbone.Collection.extend({
   url: 'http://tiy-fee-rest.herokuapp.com/collections/chrisblog',
   model: actorPost
 });
+
+var Shows = Backbone.Collection.extend({
+  model: show
+});
